a74596af4fef0adc662a62f555306a00c433e47f
hcfranck <hc_throwdown@hotmail.com>
2013-12-03 16:50:43 -0500

    SQL Equipes Done

--- AutreShits/OPDELIVER.sql
+++ AutreShits/OPDELIVER.sql
@@ -73,3 +73,20 @@ create table Statistiques
   constraint FKNUMJOUEURS Foreign Key(NumeroJoueurs) References Joueurs(NumeroJoueurs),
   constraint FKNUMMATCH Foreign Key(NumeroMatch) References Match(NumeroMatch)  
 );
+
+---------------------------------------------------------------------------------------------------------------------------------
+
+--Insertions
+
+INSERT INTO Equipes (NomEquipe,DateIntroLigue,DivisionEquipe,VilleEquipe) Values ('Canadiens' , '01-jan-1976','Division Est','Montreal');
+INSERT INTO Equipes (NomEquipe,DateIntroLigue,DivisionEquipe,VilleEquipe) Values ('Bruins' , '02-feb-1976','Division Est','Boston');
+INSERT INTO Equipes (NomEquipe,DateIntroLigue,DivisionEquipe,VilleEquipe) Values ('Senators' , '06-jun-1978','Division Est','Ottawa');
+INSERT INTO Equipes (NomEquipe,DateIntroLigue,DivisionEquipe,VilleEquipe) Values ('Sabres' , '03-jan-1980','Division Est','Buffalo');
+INSERT INTO Equipes (NomEquipe,DateIntroLigue,DivisionEquipe,VilleEquipe) Values ('Blackhawks' , '12-aug-1982','Division Ouest','Chicago');
+INSERT INTO Equipes (NomEquipe,DateIntroLigue,DivisionEquipe,VilleEquipe) Values ('Kings' , '12-dec-1985','Division Ouest','Los Angeles');
+INSERT INTO Equipes (NomEquipe,DateIntroLigue,DivisionEquipe,VilleEquipe) Values ('Canucks' , '08-nov-1986','Division Ouest','Vancouver');
+INSERT INTO Equipes (NomEquipe,DateIntroLigue,DivisionEquipe,VilleEquipe) Values ('Canadiens' , '23-jan-1989','Division Ouest','Dallas');
+
+
+
+
