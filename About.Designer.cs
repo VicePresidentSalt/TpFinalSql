8cda01d63be43e4adddca4b9602f332192921be1
hcfranck <hc_throwdown@hotmail.com>
2013-12-02 20:19:32 -0500

    Form About fini avec message d'amour

--- About.Designer.cs
+++ About.Designer.cs
@@ -28,13 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
             this.BTN_OK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // BTN_OK
             // 
             this.BTN_OK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.BTN_OK.Location = new System.Drawing.Point(190, 137);
+            this.BTN_OK.Location = new System.Drawing.Point(172, 114);
             this.BTN_OK.Name = "BTN_OK";
             this.BTN_OK.Size = new System.Drawing.Size(75, 23);
             this.BTN_OK.TabIndex = 0;
@@ -43,12 +45,21 @@
             this.BTN_OK.UseVisualStyleBackColor = true;
             this.BTN_OK.Click += new System.EventHandler(this.BTN_OK_Click);
             // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 79);
+            this.label1.TabIndex = 1;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
             // About
             // 
             this.AcceptButton = this.BTN_OK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(277, 172);
+            this.ClientSize = new System.Drawing.Size(259, 149);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.BTN_OK);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -64,5 +75,6 @@
         #endregion
 
         private System.Windows.Forms.Button BTN_OK;
+        private System.Windows.Forms.Label label1;
     }
 }
