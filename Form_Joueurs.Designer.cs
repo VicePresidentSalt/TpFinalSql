b2548dff9f6dbf2793f7cc6e89c1a904ef534178
hcfranck <hc_throwdown@hotmail.com>
2013-12-04 17:05:45 -0500

    Form_Joueur beginning ...........bitch

--- Form_Joueurs.Designer.cs
+++ Form_Joueurs.Designer.cs
@@ -28,33 +28,201 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.DGV_Joueurs = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.DGV_Joueurs)).BeginInit();
+            this.BTN_OK = new System.Windows.Forms.Button();
+            this.TB_NumeroJoueur = new System.Windows.Forms.TextBox();
+            this.TB_Nom = new System.Windows.Forms.TextBox();
+            this.TB_Prenom = new System.Windows.Forms.TextBox();
+            this.TB_NumeroMaillot = new System.Windows.Forms.TextBox();
+            this.TB_EquipeJoueur = new System.Windows.Forms.TextBox();
+            this.TB_PositionJoueur = new System.Windows.Forms.TextBox();
+            this.DTP_DateNaissance = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // DGV_Joueurs
+            // BTN_OK
             // 
-            this.DGV_Joueurs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGV_Joueurs.Location = new System.Drawing.Point(25, 22);
-            this.DGV_Joueurs.Name = "DGV_Joueurs";
-            this.DGV_Joueurs.Size = new System.Drawing.Size(449, 283);
-            this.DGV_Joueurs.TabIndex = 0;
+            this.BTN_OK.Location = new System.Drawing.Point(105, 333);
+            this.BTN_OK.Name = "BTN_OK";
+            this.BTN_OK.Size = new System.Drawing.Size(97, 25);
+            this.BTN_OK.TabIndex = 0;
+            this.BTN_OK.Text = "Ok";
+            this.BTN_OK.UseVisualStyleBackColor = true;
+            // 
+            // TB_NumeroJoueur
+            // 
+            this.TB_NumeroJoueur.Location = new System.Drawing.Point(162, 29);
+            this.TB_NumeroJoueur.Name = "TB_NumeroJoueur";
+            this.TB_NumeroJoueur.Size = new System.Drawing.Size(129, 20);
+            this.TB_NumeroJoueur.TabIndex = 1;
+            // 
+            // TB_Nom
+            // 
+            this.TB_Nom.Location = new System.Drawing.Point(162, 68);
+            this.TB_Nom.Name = "TB_Nom";
+            this.TB_Nom.Size = new System.Drawing.Size(129, 20);
+            this.TB_Nom.TabIndex = 2;
+            // 
+            // TB_Prenom
+            // 
+            this.TB_Prenom.Location = new System.Drawing.Point(162, 107);
+            this.TB_Prenom.Name = "TB_Prenom";
+            this.TB_Prenom.Size = new System.Drawing.Size(129, 20);
+            this.TB_Prenom.TabIndex = 3;
+            // 
+            // TB_NumeroMaillot
+            // 
+            this.TB_NumeroMaillot.Location = new System.Drawing.Point(162, 181);
+            this.TB_NumeroMaillot.Name = "TB_NumeroMaillot";
+            this.TB_NumeroMaillot.Size = new System.Drawing.Size(129, 20);
+            this.TB_NumeroMaillot.TabIndex = 4;
+            // 
+            // TB_EquipeJoueur
+            // 
+            this.TB_EquipeJoueur.Location = new System.Drawing.Point(162, 218);
+            this.TB_EquipeJoueur.Name = "TB_EquipeJoueur";
+            this.TB_EquipeJoueur.Size = new System.Drawing.Size(129, 20);
+            this.TB_EquipeJoueur.TabIndex = 5;
+            // 
+            // TB_PositionJoueur
+            // 
+            this.TB_PositionJoueur.Location = new System.Drawing.Point(162, 256);
+            this.TB_PositionJoueur.Name = "TB_PositionJoueur";
+            this.TB_PositionJoueur.Size = new System.Drawing.Size(129, 20);
+            this.TB_PositionJoueur.TabIndex = 6;
+            // 
+            // DTP_DateNaissance
+            // 
+            this.DTP_DateNaissance.Location = new System.Drawing.Point(162, 143);
+            this.DTP_DateNaissance.Name = "DTP_DateNaissance";
+            this.DTP_DateNaissance.Size = new System.Drawing.Size(129, 20);
+            this.DTP_DateNaissance.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 302);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Fleche Flash button here";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Numero Joueur";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Nom du Joueur";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Prénom du joueur";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Date de Naissance";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Numero de maillot";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(26, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Équipe du joueur";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(26, 259);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Position du joueur";
             // 
             // Form_Joueurs
             // 
+            this.AcceptButton = this.BTN_OK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(897, 412);
-            this.Controls.Add(this.DGV_Joueurs);
+            this.ClientSize = new System.Drawing.Size(305, 370);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DTP_DateNaissance);
+            this.Controls.Add(this.TB_PositionJoueur);
+            this.Controls.Add(this.TB_EquipeJoueur);
+            this.Controls.Add(this.TB_NumeroMaillot);
+            this.Controls.Add(this.TB_Prenom);
+            this.Controls.Add(this.TB_Nom);
+            this.Controls.Add(this.TB_NumeroJoueur);
+            this.Controls.Add(this.BTN_OK);
             this.Name = "Form_Joueurs";
             this.Text = "Form_Joueurs";
-            ((System.ComponentModel.ISupportInitialize)(this.DGV_Joueurs)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView DGV_Joueurs;
+        private System.Windows.Forms.Button BTN_OK;
+        private System.Windows.Forms.TextBox TB_NumeroJoueur;
+        private System.Windows.Forms.TextBox TB_Nom;
+        private System.Windows.Forms.TextBox TB_Prenom;
+        private System.Windows.Forms.TextBox TB_NumeroMaillot;
+        private System.Windows.Forms.TextBox TB_EquipeJoueur;
+        private System.Windows.Forms.TextBox TB_PositionJoueur;
+        private System.Windows.Forms.DateTimePicker DTP_DateNaissance;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+
     }
 }
