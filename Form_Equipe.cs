6875c55078dd03da7d511928b0f9073513c6c1c7
hcfranck <hc_throwdown@hotmail.com>
2013-12-04 13:29:28 -0500

    Still working on blob 2

--- Form_Equipe.cs
+++ Form_Equipe.cs
@@ -45,46 +45,53 @@ namespace TPFinalSQLDEVCoteFrancisStlaurentDarenKen
             FormEquipe_Ajouter Ajouter = new FormEquipe_Ajouter();
             if (Ajouter.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
+                nomFichier =Ajouter.nomFichier;
                 string sqlAjout = "insert into Equipes (NomEquipe,DateIntroLigue,LogoEquipe,DivisionEquipe,VilleEquipe)" + // Rajout de LogoEquipe dans la commande ...incomming crashs bitches !
                     " VALUES(:NomEquipe,:DateIntroLigue,:LogoEquipe,:DivisionEquipe,:VilleEquipe)";
                 try 
                 {
+                    
+                    OracleCommand oraAjout = new OracleCommand(sqlAjout, conn);
+                    
                     OracleParameter OraParaNomEquipe = new OracleParameter(":NomEquipe", OracleDbType.Varchar2, 40);
                     OracleParameter OraParamDateIntroLigue = new OracleParameter(":DateIntroLigue", OracleDbType.Date);
-                    OracleParameter OraParamLogoEquipe = new OracleParameter(":LogoEquipe", OracleDbType.Blob);  //Ajout
+                    OracleParameter OraParamLogoEquipe = new OracleParameter(":LogoEquipe", OracleDbType.Int32);  //Ajout
                     OracleParameter OraParaDivEquipe = new OracleParameter(":DivisionEquipe", OracleDbType.Varchar2, 40);
                     OracleParameter OraParaVilleEquipe = new OracleParameter(":VilleEquipe", OracleDbType.Varchar2, 40);
 
                     OraParaNomEquipe.Value = Ajouter.nomEquipe;
                     OraParamDateIntroLigue.Value = DateTime.Parse(Ajouter.dateIntroLigue);
-                    //OraParamLogoEquipe.Value = Ajouter.nomFichier;
+                    OraParamLogoEquipe.Value = Ajouter.nomFichier;
                     OraParaDivEquipe.Value = Ajouter.divisionEquipe;
                     OraParaVilleEquipe.Value = Ajouter.villeEquipe;
 
-                    OracleCommand oraAjout = new OracleCommand(sqlAjout, conn);
-
-                    // récuper le fichier nomFichier et le convertir en Byte. 
-                    //le résultat est dans buffer1
-                    // oracle stocke les images sous forme de Bytes.
-                    FileStream Streamp = new FileStream(nomFichier, FileMode.Open, FileAccess.Read);
-                    byte[] buffer1 = new byte[Streamp.Length];
-                    Streamp.Read(buffer1, 0, System.Convert.ToInt32(Streamp.Length));
-                    Streamp.Close();
-                    // ajout de la photo dans la BD.
-
-                    OraParamLogoEquipe.Value = buffer1;
-                    oraAjout.Parameters.Add(OraParamLogoEquipe);
+                    
                     oraAjout.Parameters.Add(OraParaNomEquipe);
                     oraAjout.Parameters.Add(OraParamDateIntroLigue);
                     oraAjout.Parameters.Add(OraParaDivEquipe);
                     oraAjout.Parameters.Add(OraParaVilleEquipe);
 
+                    // récuper le fichier nomFichier et le convertir en Byte. 
+                    //le résultat est dans buffer1
+                    // oracle stocke les images sous forme de Bytes.
+                    if (nomFichier != null)
+                    {
+                        FileStream Streamp = new FileStream(nomFichier, FileMode.Open, FileAccess.Read);
+                        byte[] buffer1 = new byte[Streamp.Length];
+                        Streamp.Read(buffer1, 0, System.Convert.ToInt32(Streamp.Length));
+                        Streamp.Close();
+
+                        // ajout de la photo dans la BD.
+
+                        OraParamLogoEquipe.Value = buffer1;
+                    }
+                    oraAjout.Parameters.Add(OraParamLogoEquipe);
                     oraAjout.ExecuteNonQuery();
 
                     ReloadDGV();
                 }
 
-                catch (Exception ex)
+                catch (OracleException ex)
                 {
                     MessageBox.Show(ex.Message.ToString());
                 }
