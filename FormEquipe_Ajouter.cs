b68dc964ee9636fdf3a019ad7bacdcf285456e24
hcfranck <hc_throwdown@hotmail.com>
2013-12-03 19:37:51 -0500

    Form equipe btn ajouter / modifier done
    reste testing si marche bien

---
+++ FormEquipe_Ajouter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TPFinalSQLDEVCoteFrancisStlaurentDarenKen
+{
+    public partial class FormEquipe_Ajouter : Form
+    {
+        public string nomEquipe
+        {
+            get
+            {
+                return TB_NomEquipe.Text;
+            }
+            set 
+            {
+                TB_NomEquipe.Text = value;
+            }
+        }
+        public string dateIntroLigue
+        {
+            get
+            {
+                return DTP_DateCreationEquipe.Value.ToShortDateString();
+            }
+            set
+            {
+                DTP_DateCreationEquipe.Value = DateTime.Parse(value);
+            } 
+        }
+        public string divisionEquipe
+        {
+            get
+            {
+                return TB_DivisionEquipe.Text;
+            }
+            set
+            {
+                TB_DivisionEquipe.Text = value;
+            }
+        }
+        public string villeEquipe
+        {
+            get
+            {
+                return TB_VilleEquipe.Text;
+            }
+            set
+            {
+                TB_VilleEquipe.Text = value;
+            }
+        }
+        
+        public FormEquipe_Ajouter()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Form_Equipe.Designer.cs
+++ Form_Equipe.Designer.cs
@@ -28,11 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.DGV_Equipe = new System.Windows.Forms.DataGridView();
+            this.BTN_Ajouter = new System.Windows.Forms.Button();
+            this.BTN_Modifier = new System.Windows.Forms.Button();
+            this.BTN_Delete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Equipe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGV_Equipe
+            // 
+            this.DGV_Equipe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_Equipe.Location = new System.Drawing.Point(12, 12);
+            this.DGV_Equipe.Name = "DGV_Equipe";
+            this.DGV_Equipe.Size = new System.Drawing.Size(451, 268);
+            this.DGV_Equipe.TabIndex = 0;
+            // 
+            // BTN_Ajouter
+            // 
+            this.BTN_Ajouter.Location = new System.Drawing.Point(496, 12);
+            this.BTN_Ajouter.Name = "BTN_Ajouter";
+            this.BTN_Ajouter.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Ajouter.TabIndex = 1;
+            this.BTN_Ajouter.Text = "Ajouter";
+            this.BTN_Ajouter.UseVisualStyleBackColor = true;
+            this.BTN_Ajouter.Click += new System.EventHandler(this.BTN_Ajouter_Click);
+            // 
+            // BTN_Modifier
+            // 
+            this.BTN_Modifier.Location = new System.Drawing.Point(496, 85);
+            this.BTN_Modifier.Name = "BTN_Modifier";
+            this.BTN_Modifier.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Modifier.TabIndex = 2;
+            this.BTN_Modifier.Text = "Modifier";
+            this.BTN_Modifier.UseVisualStyleBackColor = true;
+            this.BTN_Modifier.Click += new System.EventHandler(this.BTN_Modifier_Click);
+            // 
+            // BTN_Delete
+            // 
+            this.BTN_Delete.Location = new System.Drawing.Point(496, 162);
+            this.BTN_Delete.Name = "BTN_Delete";
+            this.BTN_Delete.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Delete.TabIndex = 3;
+            this.BTN_Delete.Text = "Supprimer";
+            this.BTN_Delete.UseVisualStyleBackColor = true;
+            this.BTN_Delete.Click += new System.EventHandler(this.BTN_Delete_Click);
+            // 
+            // Form_Equipe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 295);
+            this.Controls.Add(this.BTN_Delete);
+            this.Controls.Add(this.BTN_Modifier);
+            this.Controls.Add(this.BTN_Ajouter);
+            this.Controls.Add(this.DGV_Equipe);
+            this.Name = "Form_Equipe";
             this.Text = "Form_Equipe";
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Equipe)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView DGV_Equipe;
+        private System.Windows.Forms.Button BTN_Ajouter;
+        private System.Windows.Forms.Button BTN_Modifier;
+        private System.Windows.Forms.Button BTN_Delete;
     }
 }
