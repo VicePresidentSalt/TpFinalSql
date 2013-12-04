59f7b6efb464147541dee061061492a425c56df5
hcfranck <hc_throwdown@hotmail.com>
2013-12-04 12:56:14 -0500

    Still working on blob

--- Form_Equipe.Designer.cs
+++ Form_Equipe.Designer.cs
@@ -109,6 +109,7 @@
             this.Controls.Add(this.DGV_Equipe);
             this.Name = "Form_Equipe";
             this.Text = "Form_Equipe";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Equipe_FormClosing);
             this.Load += new System.EventHandler(this.FormEquipe_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DGV_Equipe)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.PB_Equipes)).EndInit();
