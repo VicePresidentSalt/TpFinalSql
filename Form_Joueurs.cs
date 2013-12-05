ae605af47696e2810ad01e0964f4e7559fd35c6c
hcfranck <hc_throwdown@hotmail.com>
2013-12-04 19:22:29 -0500

    Form_Joueurs still have work to go

--- Form_Joueurs.cs
+++ Form_Joueurs.cs
@@ -14,7 +14,7 @@ namespace TPFinalSQLDEVCoteFrancisStlaurentDarenKen
     {
         public OracleConnection conn = null;
         public Form callBackForm = null;
-        private DataSet joueurDataSet = null;
+        private DataSet joueurDataSet = new DataSet();
         
         public Form_Joueurs()
         {
@@ -36,25 +36,21 @@ namespace TPFinalSQLDEVCoteFrancisStlaurentDarenKen
         {
             
         }
-        /*
+        
         private void Getinfos()
         {
             try
             {
-                string SQL = "SELECT * FROM DISQUES";
+                string SQL = "SELECT * FROM JOUEURS";
 
                 OracleDataAdapter Oraad = new OracleDataAdapter(SQL, conn);
 
-                if (Data.Tables.Contains("Resultats"))
+                if (joueurDataSet.Tables.Contains("Joueur"))
                 {
-                    Data.Tables["Resultat"].Clear();
+                    joueurDataSet.Tables["Joueur"].Clear();
                 }
-                Oraad.Fill(Data, "Resultats");
+                Oraad.Fill(joueurDataSet, "Joueur");
                 Oraad.Dispose();
-
-                BindingSource Source = new BindingSource(Data, "Resultats");
-
-                DataDisques.DataSource = Source;
             }
             catch (Exception se)
             {
@@ -70,69 +66,64 @@ namespace TPFinalSQLDEVCoteFrancisStlaurentDarenKen
             
             if (joueurDataSet.Tables.Contains("Joueur"))
             {
-                joueurDataSet.Tables["LB_Lister_Cie"].Clear();
+                joueurDataSet.Tables["Joueur"].Clear();
+                
             }
 
-            oraDisque.Fill(monDataSet, "LB_Lister_Cie");
-            oraDisque.Dispose();
-            BindingSource maSource;
-            maSource = new BindingSource(monDataSet, "LB_Lister_Cie");
-            DGV_Disques.DataSource = maSource;
-            
-            
-            TB_NumeroJoueur.DataBindings.Add("Text", joueurDataSet, "TB_N.NoDisque");
-            TB_Nom.DataBindings.Add("Text", joueurDataSet, "LB_Lister_Cie.TitreDisque");
-            TB_Prenom.DataBindings.Add("Text", joueurDataSet, "LB_Lister_Cie.DureeMinutesDisque");
-            DTP_DateNaissance.DataBindings.Add("Calendrier", joueurDataSet, "LB_Lister_Cie.AnnéeDisque");
-            TB_NumeroMaillot.DataBindings.Add("Text", joueurDataSet, "LB_Lister_Cie.Nocie");
-            TB_EquipeJoueur.DataBindings.Add("Text", joueurDataSet, "LB_Lister_Cie.Nocie");
-            TB_PositionJoueur.DataBindings.Add("Text", joueurDataSet, "LB_Lister_Cie.Nocie");
+            oraAdapter.Fill(joueurDataSet, "Joueur");
+            oraAdapter.Dispose();
+
+            UpdateTextBox();
             
         }
         private void UpdateTextBox()
         {
-            TB_NoDisque.DataBindings.Add("text", Data, "Resultats.NoDisque");
-            TB_Titre.DataBindings.Add("text", Data, "Resultats.TitreDisque");
-            TB_Duree.DataBindings.Add("text", Data, "Resultats.DureeMinuteDisque");
-            TB_Annee.DataBindings.Add("text", Data, "Resultats.AnneeDisque");
-            TB_NOCIE.DataBindings.Add("text", Data, "Resultats.Nocie");
+            TB_NumeroJoueur.DataBindings.Add("Text", joueurDataSet, "Joueur.NumeroJoueurs");
+            TB_NomJoueur.DataBindings.Add("Text", joueurDataSet, "Joueur.NomJoueurs");
+            TB_PrenomJoueur.DataBindings.Add("Text", joueurDataSet, "Joueur.PrenomJoueur");
+            DTP_DateNaissanceJoueur.DataBindings.Add("Calendrier", joueurDataSet, "Joueur.DateNaissance");
+            TB_NumeroMaillotJoueur.DataBindings.Add("Text", joueurDataSet, "Joueur.NumeroMaillot");
+            TB_EquipeJoueur.DataBindings.Add("Text", joueurDataSet, "Joueur.EquipeJoueur");
+            TB_PositionJoueur.DataBindings.Add("Text", joueurDataSet, "Joueur.PositionJoueur");
         }
         private void ClearBindings()
         {
-            TB_NoDisque.DataBindings.Clear();
-            TB_NoDisque.Clear();
-            TB_Titre.DataBindings.Clear();
-            TB_Titre.Clear();
-            TB_Duree.DataBindings.Clear();
-            TB_Duree.Clear();
-            TB_Annee.DataBindings.Clear();
-            TB_Annee.Clear();
-            TB_NOCIE.DataBindings.Clear();
-            TB_NOCIE.Clear();
-
+            TB_NumeroJoueur.DataBindings.Clear();
+            TB_NumeroJoueur.Clear();
+            TB_NomJoueur.DataBindings.Clear();
+            TB_NomJoueur.Clear();
+            TB_PrenomJoueur.DataBindings.Clear();
+            TB_PrenomJoueur.Clear();
+            DTP_DateNaissanceJoueur.DataBindings.Clear();
+            DTP_DateNaissanceJoueur.Value = new DateTime();
+            TB_NumeroMaillotJoueur.DataBindings.Clear();
+            TB_NumeroMaillotJoueur.Clear();
+            TB_EquipeJoueur.DataBindings.Clear();
+            TB_EquipeJoueur.Clear();
+            TB_PositionJoueur.DataBindings.Clear();
+            TB_PositionJoueur.Clear();
         }
         private void premier_Click(object sender, EventArgs e)
         {
-            this.BindingContext[Data, "Resultats"].Position = 0;
+            //this.BindingContext[Data, "Resultats"].Position = 0;
         }
 
         private void dernier_Click(object sender, EventArgs e)
         {
-            this.BindingContext[Data, "Resultats"].Position =
-            this.BindingContext[Data, "Resultats"].Count - 1;
+            //this.BindingContext[Data, "Resultats"].Position =
+            //this.BindingContext[Data, "Resultats"].Count - 1;
 
         }
         private void suivant_Click(object sender, EventArgs e)
         {
-            this.BindingContext[Data, "Resultats"].Position++;
+            //this.BindingContext[Data, "Resultats"].Position++;
 
         }
 
         private void precedent_Click(object sender, EventArgs e)
         {
-            this.BindingContext[Data, "Resultats"].Position--;
+            //this.BindingContext[Data, "Resultats"].Position--;
         }
-         */
     }
          
              
