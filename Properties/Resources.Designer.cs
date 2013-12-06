47fdc405a24bffc514b5d517f5cb498666b9bb22
hcfranck <hc_throwdown@hotmail.com>
2013-12-06 11:05:08 -0500

    Commencer Form_Match

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -79,5 +79,25 @@ namespace TPFinalSQLDEVCoteFrancisStlaurentDarenKen.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Logo___Versus {
+            get {
+                object obj = ResourceManager.GetObject("Logo_-_Versus", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Logo___Versus1 {
+            get {
+                object obj = ResourceManager.GetObject("Logo_-_Versus1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
